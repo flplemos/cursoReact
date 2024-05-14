3d2d1c2de3085b1af7fe3b8d0b6f2b62ff7cec94
Felipe Lemos <139652554+flplemos@users.noreply.github.com>
2024-05-14 16:09:01 -0300

    update

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Condicional from './components/Condicional';
+
 
 function App() {
 
-  const nome = "Maria";
-
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase/>
-      <Frase/>
-     <SayMyName nome="Felipe" />
-     <SayMyName nome={nome}/>
-     <Pessoa nome="Kurt" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"/>
-     <List/>
+      <h1>Renderização condicional</h1>
+      <Condicional/>
     </div>
   );
 }
---
+++ meu-projeto/src/components/Form.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+function Form() {
+
+    function cadastrarUsuario(e){
+        e.preventDefault()
+        console.log(`Usuário ${name} foi cadastrado com a senha ${password}`)
+        console.log("Cadastrou o usuário")
+    }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+
+    return(
+        <div>
+            <h1>Meu cadastro</h1> 
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                    type="text" 
+                    placeholder="Digite o seu nome" 
+                    id="name" 
+                    name="name" 
+                    onChange={(e) => setName(e.target.value)}/>
+                    
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                    type="password" 
+                    placeholder="Digite a sua senha" id="password" 
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar" />
+                </div>
+            </form>
+        </div>
+    )
+
+}
+
+export default Form
